8c0ed78167e44ba3389f0c0dfdf5ce98a1de55bf
Md Ismail Hossain <ismailtex043@gmail.com>
2021-02-08 14:30:02 +0600

    branch commit

--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
-STATIC_ROOT = [os.path.join(BASE_DIR,'media')]
+STATIC_ROOT = [os.path.join(BASE_DIR,'staticfiles')]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
