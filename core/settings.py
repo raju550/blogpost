2059facec20af80d735b06fc51e54720098ab30b
raju <ismailtex043@gmail.com>
2021-02-09 17:55:25 +0600

    branch commit

--- core/settings.py
+++ core/settings.py
@@ -54,11 +54,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'core.urls'
-# CORS_ORIGIN_WHITELIST = [
-#     'http://localhost:3000',
-# ]
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
 
 
 TEMPLATES = [
